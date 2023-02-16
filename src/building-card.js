9db7f2b1f26f10de72b7d32a8a3139cc2b3a7ddc
FrankPSU27 <122505447+FrankPSU27@users.noreply.github.com>
2023-02-16 11:17:05 -0500

    Building Time

--- src/building-card.js
+++ src/building-card.js
@@ -2,86 +2,90 @@ import { LitElement, html, css } from 'lit';
 
 const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
-class BuildingCard extends LitElement {
-  static properties = {
-    header: { type: String },
+export class BuildingCard extends LitElement {
+  static get properties() {
+    return {
+    name: {
+      type: String,
+      reflect: true
+    },
+    }
   }
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--building-card-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
+  static get styles() {
+    return css`
+      .wrapper {
+        width: 400px;
+        border: 2px solid black;
+        display: inline-flex;
       }
-      to {
-        transform: rotate(360deg);
+      
+      .image {
+        width: 400px;
       }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
+      
+      .header {
+        text-align: center;
+        font-weight: bold;
+        font-size: 2rem;
+      }
+      
+      .header h3 {
+        transition: .3s ease-in-out all;
+        margin: 16px;
+        font-style: normal;
+      }
+      
+      .details summary {
+        fount-size: 20px;
+        fount-weight: bold;
+      }
+      .button2{
+        text-transform: uppercase;
+        background-color: blue;
+        color: white;
+        padding: 8px 8px;
+        display: none;
+        padding: 8px 16px;
+        margin-bottom: 10px;
+      }
+      
+       @media (max-width: 800px) and (min-width: 500px)  {
+          .button2 {
+            display: inline-block;
+          } 
+        }
+      
+      body{
+        background-color: grey;
+      }
+    }`;
+  }
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.name = "WestGate";
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/BuildingCard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
-    `;
+  <div class="wrapper">
+    <div class="container">
+    <img class="image" src="https://i4-wp.ist.psu.edu/gencyber/wp-content/uploads/sites/17/2018/03/westgatebuilding-256tord.jpg"/>
+    <div class="header">
+      <h3>${this.name}</h3>
+    </div>
+    <details class="details">
+      <summary>INFO</summary>
+      <div>
+        <ul>
+          <li>The college of IST at Penn State is the perfect place for yong srudents to learn IT skills so they can function in society. 
+            If you would like to know more about the opritunities offered at PSU click the detals button bellow.</li>
+         </ul>
+      </div>
+      </details>
+      <a href="https://hax.psu.edu" class="button2">Detals</a>
+  </div>`;
   }
 }
 
